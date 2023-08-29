549812efb54337a54cdaa360a477266809d29746
Sophia Zhang <ssz.windyskies@gmail.com>
2023-08-29 12:41:14 -0700

    countCharacters
    wrote countCharacters (String fileName), made tester main, tester file, and minor update to one of the methods (see code comments)

--- FileWriter.java
+++ FileWriter.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class FileWriter {
-    public String read(String filename) throws IOException{
+    public static String read(String filename) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader(filename));
         String str = "";
         while(br.ready()){
-            str+=br.read();
+            str+= (char) br.read(); //hey og person, sophia also typecasted this into char for you
         }
         br.close();
         return str;
@@ -19,5 +19,25 @@ public class FileWriter {
      PrintWriter pw = new PrintWriter(fileName);
      pw.write(s);
      pw.close();
+    }
+
+    //Sophia's nice edits
+    public static int countCharacters (String fileName) throws IOException
+    {
+        BufferedReader br = new BufferedReader (new FileReader (fileName));
+        int characterCount = 0;
+        while (br.ready())
+        {
+            characterCount ++;
+            br.read();
+        }
+        br.close();
+        return characterCount;
+    }
+
+    public static void main (String [] args) throws IOException
+    {
+        System.out.println (read ("FileWriterTester.txt")); //works
+        System.out.println (countCharacters ("FileWriterTester.txt")); //works
     }
 }
